f893c2f6aa585a4f71edd9c49710283d76732028
Luiz Felipe <73553939+FelipeDevMelo@users.noreply.github.com>
2020-11-20 13:26:38 -0300

    initial commit

---
+++ Assets/Prefabs/New Folder/Salva.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Salva : MonoBehaviour
+{
+     public Text txt;
+     public InputField caixa;
+     private float testf;
+    
+    void Start()
+    {
+        txt.text = PlayerPrefs.GetFloat("pontos").ToString();
+    }
+    public void SalvarFloat()
+    {
+        testf = float.Parse(caixa.text);
+        PlayerPrefs.SetFloat("pontos", testf);
+       
+    }
+    
+    void Update()
+    {
+        
+    }
+}
